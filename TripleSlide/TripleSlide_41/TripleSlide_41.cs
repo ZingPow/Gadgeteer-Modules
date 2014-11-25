02eac6b7b1541eeeea52de93ffac816f90e21387
ZingPow <mcneillb@zingpow.ca>
2014-11-25 14:31:18 -0700

    Initial commit of Solder Monkey Triple Slider Gadgeteer Module Drivers for 4.1, 4.2 and 4.3

---
+++ TripleSlide/TripleSlide_41/TripleSlide_41.cs
@@ -0,0 +1,52 @@
+﻿using Gadgeteer.Interfaces;
+
+namespace Gadgeteer.Modules.SolderMonkey
+{
+    /// <summary>
+    ///     A TripleSlide module for Microsoft .NET Gadgeteer
+    /// </summary>
+    public class TripleSlide : Module
+    {
+        /// <summary />
+        public enum sliderID
+        {
+            slider1 = 1,
+            slider2 = 2,
+            slider3 = 3,
+        }
+
+        private readonly AnalogInput Slide1;
+        private readonly AnalogInput Slide2;
+        private readonly AnalogInput Slide3;
+
+        /// <summary />
+        /// <param name="socketNumber">The socket that this module is plugged in to.</param>
+        public TripleSlide(int socketNumber)
+        {
+            Socket socket = Socket.GetSocket(socketNumber, true, this, null);
+            Slide1 = new AnalogInput(socket, Socket.Pin.Three, this);
+            Slide2 = new AnalogInput(socket, Socket.Pin.Four, this);
+            Slide3 = new AnalogInput(socket, Socket.Pin.Five, this);
+        }
+
+        /// <summary />
+        /// <param name="ID" />
+        /// <returns>
+        ///     Returns the current position of the slide from 0.0 to 1.0
+        /// </returns>
+        public double ReadSliderPosition(sliderID ID)
+        {
+            switch (ID)
+            {
+                case sliderID.slider1:
+                    return Slide1.ReadProportion();
+                case sliderID.slider2:
+                    return Slide2.ReadProportion();
+                case sliderID.slider3:
+                    return Slide3.ReadProportion();
+                default:
+                    return 0.0;
+            }
+        }
+    }
+}
---
+++ TripleSlide/TripleSlide_43/TripleSlide_43.cs
@@ -0,0 +1,52 @@
+﻿using Gadgeteer.SocketInterfaces;
+
+namespace Gadgeteer.Modules.SolderMonkey
+{
+    /// <summary>
+    ///     A TripleSlide module for Microsoft .NET Gadgeteer
+    /// </summary>
+    public class TripleSlide : Module
+    {
+        /// <summary />
+        public enum sliderID
+        {
+            Slider1 = 1,
+            Slider2 = 2,
+            Slider3 = 3,
+        }
+
+        private readonly AnalogInput _slide1;
+        private readonly AnalogInput _slide2;
+        private readonly AnalogInput _slide3;
+
+        /// <summary />
+        /// <param name="socketNumber">The socket that this module is plugged in to.</param>
+        public TripleSlide(int socketNumber)
+        {
+            Socket socket = Socket.GetSocket(socketNumber, true, this, null);
+            _slide1 = AnalogInputFactory.Create(socket, Socket.Pin.Three, this);
+            _slide2 = AnalogInputFactory.Create(socket, Socket.Pin.Four, this);
+            _slide3 = AnalogInputFactory.Create(socket, Socket.Pin.Five, this);
+        }
+
+        /// <summary />
+        /// <param name="ID" />
+        /// <returns>
+        ///     Returns the current position of the slide from 0.0 to 1.0
+        /// </returns>
+        public double ReadSliderPosition(sliderID ID)
+        {
+            switch (ID)
+            {
+                case sliderID.Slider1:
+                    return _slide1.ReadProportion();
+                case sliderID.Slider2:
+                    return _slide2.ReadProportion();
+                case sliderID.Slider3:
+                    return _slide3.ReadProportion();
+                default:
+                    return 0.0;
+            }
+        }
+    }
+}
